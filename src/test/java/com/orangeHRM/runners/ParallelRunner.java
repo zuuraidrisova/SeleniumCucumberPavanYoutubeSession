dc56781c6c5a13367c35d9590a874c9cf6a7c5bd
Zuura Idrisova <zuuraidrisova@gmail.com>
2021-01-26 20:23:05 -0500

    Selenium Cucumber Junit Pavan Youtube Session Second commit

---
+++ src/test/java/com/orangeHRM/runners/ParallelRunner.java
@@ -0,0 +1,14 @@
+package com.orangeHRM.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "json:target/cucumber1.json",
+        features = "src/test/resources/features",
+        glue = "com/orangeHRM/stepDefinitions"
+)
+public class ParallelRunner {
+}
